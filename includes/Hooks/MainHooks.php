7f97a92d13c7fff2da44fcf2c5106dfef89bcd69
alistair3149 <alistair31494322@gmail.com>
2025-05-31 19:09:47 -0400

    feat: show error message when user is not allowed to unscope
    Also various clean ups to the handleTag method

--- includes/Hooks/MainHooks.php
+++ includes/Hooks/MainHooks.php
@@ -1,15 +1,19 @@
 <?php
 
+declare( strict_types=1 );
+
 namespace MediaWiki\Extension\TemplateStylesExtender\Hooks;
 
-use MediaWiki\Extension\TemplateStyles\Hooks;
+use MediaWiki\Extension\TemplateStyles\Hooks as TemplateStylesHooks;
 use MediaWiki\Extension\TemplateStylesExtender\TemplateStylesExtender;
 use MediaWiki\Hook\ParserFirstCallInitHook;
 use MediaWiki\MediaWikiServices;
+use MediaWiki\Parser\Parser;
+use MediaWiki\Parser\ParserOptions;
+use MediaWiki\Parser\PPFrame;
+use MediaWiki\Revision\RevisionRecord;
+use MediaWiki\Title\Title;
 use MediaWiki\User\UserIdentity;
-use MWException;
-use Parser;
-use PPFrame;
 
 /**
  * phpcs:disable MediaWiki.NamingConventions.LowerCamelFunctionsName.FunctionName
@@ -17,10 +21,10 @@ use PPFrame;
 class MainHooks implements ParserFirstCallInitHook {
 
 	/**
-	 * @param Parser $parser
-	 * @throws MWException
+	 * @inheritDoc
 	 */
 	public function onParserFirstCallInit( $parser ) {
+		// Hijack the templatestyles tag
 		$parser->setHook( 'templatestyles', [ __CLASS__, 'handleTag' ] );
 	}
 
@@ -31,54 +35,80 @@ class MainHooks implements ParserFirstCallInitHook {
 	 * Note this is a potentially expensive operation, as a lookup for the user of the current revision is done.
 	 * The unscoping will only happen, if the editor of the current revision has the rights to do so
 	 *
-	 * @param string $text
-	 * @param string[] $params
-	 * @param Parser $parser
-	 * @param PPFrame $frame
-	 * @return string
 	 * @see Hooks::handleTag()
 	 */
-	public static function handleTag( $text, $params, $parser, $frame ): string {
-		$getOutput = static fn() => Hooks::handleTag( $text, $params, $parser, $frame );
+	public static function handleTag( ?string $text, array $params, Parser $parser, PPFrame $frame ): string {
+		$getOutput = static fn() => TemplateStylesHooks::handleTag( $text, $params, $parser, $frame );
+		$options = self::getParserOptions( $parser );
 
 		if (
 			!isset( $params['wrapclass'] ) ||
-			$parser->getOptions() === null ||
+			$options === null ||
 			!TemplateStylesExtender::getConfigValue( 'TemplateStylesExtenderEnableUnscopingSupport' )
 		) {
 			return $getOutput();
 		}
 
-		// 'wrapclass' option is set...
+		$services = MediaWikiServices::getInstance();
 
-		/** @var \ParserOptions $options - Fix typehint */
-		$options = $parser->getOptions();
-		$wrapClass = $options->getWrapOutputClass();
+		$title = self::getTitle( $frame );
+		$rev = self::getRevision( $services, $title );
 
-		$permission = TemplateStylesExtender::getConfigValue( 'TemplateStylesExtenderUnscopingPermission' );
+		// Should not happen
+		if ( $rev === null ) {
+			return $getOutput(); 
+		}
 
-		$rev = MediaWikiServices::getInstance()->getRevisionLookup()->getRevisionByTitle( $frame->getTitle() );
-
-		if ( $rev === null || $rev->getUser() === null ) {
+		$user = self::getUserIdentity( $rev );
+		// Should not happen
+		if ( $user === null ) {
 			return $getOutput();
 		}
 
-		/** @var UserIdentity $user - Fix typehint */
-		$user = $rev->getUser();
-		$user = MediaWikiServices::getInstance()->getUserFactory()->newFromUserIdentity( $user );
-		$permissionManager = MediaWikiServices::getInstance()->getPermissionManager();
-
-		$userCan = $permissionManager->userHasRight( $user, $permission )
-			|| $permissionManager->userCan( $permission, $user, $frame->getTitle() );
-
-		// If the editor of the last revision is allowed to change the wrapclass, set it
-		if ( $userCan ) {
-			$options->setOption( 'wrapclass', $params['wrapclass'] );
+		if ( !self::isUserAllowedToUnscope( $services, $user, $title ) ) {
+			return self::formatTagError( $parser, [
+				'templatestylesextender-unscoping-no-permission'
+			] ) . $getOutput();
 		}
 
-		$out = Hooks::handleTag( $text, $params, $parser, $frame );
+		$wrapClass = $options->getWrapOutputClass();
+		$options->setOption( 'wrapclass', $params['wrapclass'] );
+
+		$out = TemplateStylesHooks::handleTag( $text, $params, $parser, $frame );
 		$options->setOption( 'wrapclass', $wrapClass );
 
 		return $out;
+	}
+
+	private static function getParserOptions( Parser $parser ): ?ParserOptions {
+		return $parser->getOptions();
+	}
+
+	private static function getTitle( PPFrame $frame ): Title {
+		return $frame->getTitle();
+	}
+
+	private static function getRevision( MediaWikiServices $services, Title $title ): ?RevisionRecord {
+		return $services->getRevisionLookup()->getRevisionByTitle( $title );
+	}
+
+	private static function getUserIdentity( RevisionRecord $rev ): ?UserIdentity {
+		return $rev->getUser();
+	}
+
+	private static function isUserAllowedToUnscope( MediaWikiServices $services, UserIdentity $user, Title $title ): bool {
+		$permissionManager = $services->getPermissionManager();
+		$permission = TemplateStylesExtender::getConfigValue( 'TemplateStylesExtenderUnscopingPermission' );
+
+		return $permissionManager->userHasRight( $user, $permission )
+			|| $permissionManager->userCan( $permission, $user, $title );
+	}
+
+	/** @inheritDoc TemplateStylesHooks::formatTagError() */
+	private static function formatTagError( Parser $parser, array $msg ): string {
+		$parser->addTrackingCategory( 'templatestyles-page-error-category' );
+		return '<strong class="error">' .
+			wfMessage( ...$msg )->inContentLanguage()->parse() .
+			'</strong>';
 	}
 }
