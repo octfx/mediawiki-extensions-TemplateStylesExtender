ba15ad3f697c4278c67d3a79440c24500154cbe6
alistair3149 <alistair31494322@gmail.com>
2025-06-03 04:11:43 -0400

    feat: reimplement relative color syntax
    All the specs should be implemented except for nesting relative colors

--- includes/MatcherFactoryExtender.php
+++ includes/MatcherFactoryExtender.php
@@ -70,82 +70,159 @@ class MatcherFactoryExtender extends MatcherFactory {
 	/**
 	 * Partially implements CSS Color Module Level 4 and 5
 	 *
-	 * TODO: Add support for modern syntax
-	 * TODO: Add new color functions
-	 * TODO: Tighten up relative color syntax
-	 *
 	 * @return Matcher|Matcher[]
 	 */
 	protected function colorFuncs() {
-		if ( !isset( $this->cache[__METHOD__] ) ) {
-			// Allow variables in color functions
-			$var = $this->varEnabled
-				? new FunctionMatcher( 'var', new CustomPropertyMatcher() )
-				: new NothingMatcher();
-
-			// CSS data types
-			$n = new Alternative( [ $this->number(), $var ] );
-			$p = new Alternative( [ $this->percentage(), $var ] );
-			$a = new Alternative( [ $this->angle(), $var ] );
-			$alphaValue = new Alternative( [ $n, $p ] );
-			$hue = new Alternative( [ $n, $a ] );
-
-			$none = new KeywordMatcher( [ 'none' ] );
-			$nPNone = new Alternative( [ $n, $p, $none ] );
-			$hueNone = new Alternative( [ $hue, $none ] );
-
-			$colorSpaceParams = new Alternative( [
-				// <predefined-rgb-params>
-				new Juxtaposition( [
-					new KeywordMatcher( [
-						'srgb', 'srgb-linear', 'display-p3', 'a98-rgb', 'prophoto-rgb',
-						'rec2020', 'rec2100-pq', 'rec2100-hlg', 'rec2100-linear'
-					] ),
-					Quantifier::count( $nPNone, 3, 3 ),
-				] ),
-				// <xyz-params> 
-				new Juxtaposition( [
-					new KeywordMatcher( [ 'xyz', 'xyz-d50', 'xyz-d65' ] ),
-					Quantifier::count( $nPNone, 3, 3 ),
-				] ),
-			] );
-
-			// $colorWord = new Alternative( [ $this->colorWords(), $var ] );
-			$optionalAlpha = Quantifier::optional( new Juxtaposition( [ new DelimMatcher( '/' ), new Alternative( [ $alphaValue, $none ] ) ] ) );
-			$optionalLegacyAlpha = Quantifier::optional( $alphaValue );
-
-			// Color functions
-			$rgb = new Alternative( [
-				// <legacy-rgb-syntax>
-				new Juxtaposition( [ Quantifier::hash( $p, 3, 3 ), $optionalLegacyAlpha ], true ),
-				new Juxtaposition( [ Quantifier::hash( $n, 3, 3 ), $optionalLegacyAlpha ], true ),
-				// <modern-rgb-syntax>
-				new Juxtaposition( [ Quantifier::count( $nPNone, 3, 3 ), $optionalAlpha ] ),
-			] );
-			$hsl = new Alternative( [
-				// <legacy-hsl-syntax>
-				new Juxtaposition( [ $hue, $p, $p, $optionalLegacyAlpha ], true ),
-				// <modern-hsl-syntax>
-				new Juxtaposition( [ $hueNone, $nPNone, $nPNone, $optionalAlpha ] ),
-			] );
-			$hwb = new Juxtaposition( [ $hueNone, $nPNone, $nPNone, $optionalAlpha ] );
-			$lab = new Juxtaposition( [ $nPNone, $nPNone, $nPNone, $optionalAlpha ] );
-			$lch = new Juxtaposition( [ $nPNone, $nPNone, $hueNone, $optionalAlpha ] );
-			$color = new Juxtaposition( [ $colorSpaceParams, $optionalAlpha ] );
-
-			$this->cache[__METHOD__] = [
-				new FunctionMatcher( 'rgb', $rgb ),
-				new FunctionMatcher( 'rgba', $rgb ),
-				new FunctionMatcher( 'hsl', $hsl ),
-				new FunctionMatcher( 'hsla', $hsl ),
-				new FunctionMatcher( 'hwb', $hwb ),
-				new FunctionMatcher( 'lab', $lab ),
-				new FunctionMatcher( 'lch', $lch ),
-				new FunctionMatcher( 'oklab', $lab ),
-				new FunctionMatcher( 'oklch', $lch ),
-				new FunctionMatcher( 'color', $color )
-			];
+		if ( isset( $this->cache[__METHOD__] ) ) {
+			return $this->cache[__METHOD__];
 		}
+
+		// Common var matcher
+		$var = $this->varEnabled
+			? new FunctionMatcher( 'var', new CustomPropertyMatcher() )
+			: new NothingMatcher();
+
+		$n = new Alternative( [ $this->number(), $var ] );
+		$p = new Alternative( [ $this->percentage(), $var ] );
+		$a = new Alternative( [ $this->angle(), $var ] );
+		$nP = new Alternative( [ $n, $p ] );
+		$hueWithVar = new Alternative( [ $n, $a ] );
+
+		$none = new KeywordMatcher( [ 'none' ] );
+		$nPNone = new Alternative( [ $n, $p, $none ] );
+		$hueNone = new Alternative( [ $hueWithVar, $none ] );
+
+		// Colorspace keywords
+		$predefinedRgb = new KeywordMatcher( [
+			'srgb', 'srgb-linear', 'display-p3', 'a98-rgb', 'prophoto-rgb',
+			'rec2020', 'rec2100-pq', 'rec2100-hlg', 'rec2100-linear'
+		] );
+		$xyzSpace = new KeywordMatcher( [ 'xyz', 'xyz-d50', 'xyz-d65' ] );
+
+		// Alpha matchers
+		$optionalAlpha = Quantifier::optional( new Juxtaposition(
+			[ new DelimMatcher( '/' ), new Alternative( [ $nP, $none ] ) ]
+		) );
+		$optionalLegacyAlpha = Quantifier::optional( $nP );
+
+		// Absolute color syntaxes
+		$rgbSyntax = $this->buildRgbSyntax( $n, $p, $nPNone, $optionalAlpha, $optionalLegacyAlpha );
+		$hslSyntax = $this->buildHslSyntax( $hueWithVar, $p, $hueNone, $nPNone, $optionalAlpha, $optionalLegacyAlpha );
+		$hwbSyntax = $this->buildStandardColorSyntax( [ $hueNone, $nPNone, $nPNone ], $optionalAlpha );
+		$labSyntax = $this->buildStandardColorSyntax( [ $nPNone, $nPNone, $nPNone ], $optionalAlpha );
+		$lchSyntax = $this->buildStandardColorSyntax( [ $nPNone, $nPNone, $hueNone ], $optionalAlpha );
+
+		$colorSpaceParamsForColorFunc = new Alternative( [
+			new Juxtaposition( [
+				new Alternative( [ $predefinedRgb, $xyzSpace ] ),
+				Quantifier::count( $nPNone, 3, 3 ),
+			] )
+		] );
+		$colorFuncSyntax = $this->buildStandardColorSyntax( [ $colorSpaceParamsForColorFunc ], $optionalAlpha );
+
+		$absoluteColorFuncs = [
+			new FunctionMatcher( 'rgb', $rgbSyntax ),
+			new FunctionMatcher( 'rgba', $rgbSyntax ),
+			new FunctionMatcher( 'hsl', $hslSyntax ),
+			new FunctionMatcher( 'hsla', $hslSyntax ),
+			new FunctionMatcher( 'hwb', $hwbSyntax ),
+			new FunctionMatcher( 'lab', $labSyntax ),
+			new FunctionMatcher( 'lch', $lchSyntax ),
+			new FunctionMatcher( 'oklab', $labSyntax ),
+			new FunctionMatcher( 'oklch', $lchSyntax ),
+			new FunctionMatcher( 'color', $colorFuncSyntax )
+		];
+
+		// Relative color syntax components
+		$originColor = new Alternative( [
+			$this->colorWords(),
+			$this->colorHex(),
+			$var,
+			...$absoluteColorFuncs
+		] );
+
+		$optionalAlphaCalc = new Alternative( [
+			$optionalAlpha,
+			Quantifier::optional( new Juxtaposition( [
+				new DelimMatcher( '/' ),
+				$this->calc( new KeywordMatcher( [ 'alpha' ] ), 'number' )
+			] ) )
+		] );
+
+		// Component definitions for relative colors
+		$relativeRgbComponents = [
+			$this->createRelativeColorChannel( 'r', $nPNone ),
+			$this->createRelativeColorChannel( 'g', $nPNone ),
+			$this->createRelativeColorChannel( 'b', $nPNone ),
+		];
+		$relativeHslComponents = [
+			$this->createRelativeColorChannel( 'h', $hueNone ),
+			$this->createRelativeColorChannel( 's', $nPNone ),
+			$this->createRelativeColorChannel( 'l', $nPNone ),
+		];
+		$relativeHwbComponents = [
+			$this->createRelativeColorChannel( 'h', $hueNone ),
+			$this->createRelativeColorChannel( 'w', $nPNone ),
+			$this->createRelativeColorChannel( 'b', $nPNone ),
+		];
+		$relativeLabComponents = [ // For lab and oklab
+			$this->createRelativeColorChannel( 'l', $nPNone ),
+			$this->createRelativeColorChannel( 'a', $nPNone ),
+			$this->createRelativeColorChannel( 'b', $nPNone ),
+		];
+		$relativeLchComponents = [ // For lch and oklch
+			$this->createRelativeColorChannel( 'l', $nPNone ),
+			$this->createRelativeColorChannel( 'c', $nPNone ),
+			$this->createRelativeColorChannel( 'h', $hueNone ),
+		];
+		$relativeColorFuncColorComponents = [
+			new Alternative( [
+				new Juxtaposition( [
+					$predefinedRgb,
+					$this->createRelativeColorChannel( 'r', $nPNone ),
+					$this->createRelativeColorChannel( 'g', $nPNone ),
+					$this->createRelativeColorChannel( 'b', $nPNone ),
+				] ),
+				new Juxtaposition( [
+					$xyzSpace,
+					$this->createRelativeColorChannel( 'x', $nPNone ),
+					$this->createRelativeColorChannel( 'y', $nPNone ),
+					$this->createRelativeColorChannel( 'z', $nPNone ),
+				] )
+			] )
+		];
+
+		$relativeColorFuncs = [
+			new FunctionMatcher( 'rgb',
+				$this->buildRelativeColorSyntax( $originColor, $relativeRgbComponents, $optionalAlphaCalc )
+			),
+			new FunctionMatcher( 'hsl',
+				$this->buildRelativeColorSyntax( $originColor, $relativeHslComponents, $optionalAlphaCalc )
+			),
+			new FunctionMatcher( 'hwb',
+				$this->buildRelativeColorSyntax( $originColor, $relativeHwbComponents, $optionalAlphaCalc )
+			),
+			new FunctionMatcher( 'lab',
+				$this->buildRelativeColorSyntax( $originColor, $relativeLabComponents, $optionalAlphaCalc )
+			),
+			new FunctionMatcher( 'lch',
+				$this->buildRelativeColorSyntax( $originColor, $relativeLchComponents, $optionalAlphaCalc )
+			),
+			new FunctionMatcher( 'oklab',
+				$this->buildRelativeColorSyntax( $originColor, $relativeLabComponents, $optionalAlphaCalc )
+			),
+			new FunctionMatcher( 'oklch',
+				$this->buildRelativeColorSyntax( $originColor, $relativeLchComponents, $optionalAlphaCalc )
+			),
+			new FunctionMatcher( 'color',
+				$this->buildRelativeColorSyntax( $originColor, $relativeColorFuncColorComponents, $optionalAlphaCalc )
+			),
+		];
+
+		$this->cache[__METHOD__] = [
+			...$absoluteColorFuncs,
+			...$relativeColorFuncs
+		];
 
 		return $this->cache[__METHOD__];
 	}
@@ -177,6 +254,106 @@ class MatcherFactoryExtender extends MatcherFactory {
 		] );
 
 		return $this->cache[__METHOD__];
+	}
+
+	/**
+	 * Helper to build the syntax for rgb() and rgba() functions.
+	 * @param Matcher $n Number matcher (including var)
+	 * @param Matcher $p Percentage matcher (including var)
+	 * @param Matcher $nPNone Number, percentage, or none matcher (including var)
+	 * @param Matcher $optionalAlpha Modern alpha syntax matcher
+	 * @param Matcher $optionalLegacyAlpha Legacy alpha syntax matcher
+	 * @return Alternative
+	 */
+	protected function buildRgbSyntax(
+		Matcher $n,
+		Matcher $p,
+		Matcher $nPNone,
+		Matcher $optionalAlpha,
+		Matcher $optionalLegacyAlpha
+	): Alternative {
+		return new Alternative( [
+			// <legacy-rgb-syntax>
+			new Juxtaposition( [ Quantifier::hash( $p, 3, 3 ), $optionalLegacyAlpha ], true ),
+			new Juxtaposition( [ Quantifier::hash( $n, 3, 3 ), $optionalLegacyAlpha ], true ),
+			// <modern-rgb-syntax>
+			new Juxtaposition( [ Quantifier::count( $nPNone, 3, 3 ), $optionalAlpha ] ),
+		] );
+	}
+
+	/**
+	 * Helper to build the syntax for hsl() and hsla() functions.
+	 * @param Matcher $hueWithVar Hue matcher (number, angle, including var)
+	 * @param Matcher $p Percentage matcher (including var)
+	 * @param Matcher $hueNone Hue or none matcher (including var)
+	 * @param Matcher $nPNone Number, percentage, or none matcher (including var)
+	 * @param Matcher $optionalAlpha Modern alpha syntax matcher
+	 * @param Matcher $optionalLegacyAlpha Legacy alpha syntax matcher
+	 * @return Alternative
+	 */
+	protected function buildHslSyntax(
+		Matcher $hueWithVar,
+		Matcher $p,
+		Matcher $hueNone,
+		Matcher $nPNone,
+		Matcher $optionalAlpha,
+		Matcher $optionalLegacyAlpha
+	): Alternative {
+		return new Alternative( [
+			// <legacy-hsl-syntax>
+			new Juxtaposition( [ $hueWithVar, $p, $p, $optionalLegacyAlpha ], true ),
+			// <modern-hsl-syntax>
+			new Juxtaposition( [ $hueNone, $nPNone, $nPNone, $optionalAlpha ] ),
+		] );
+	}
+
+	/**
+	 * Helper to build standard color syntax (components + optional alpha).
+	 * @param Matcher[] $components
+	 * @param Matcher $optionalAlpha
+	 * @return Juxtaposition
+	 */
+	protected function buildStandardColorSyntax(array $components, Matcher $optionalAlpha): Juxtaposition {
+		return new Juxtaposition( [ ...$components, $optionalAlpha ] );
+	}
+
+	/**
+	 * Helper to create a relative color channel matcher.
+	 * @param string $channel Character representing the channel (e.g., 'r', 'h')
+	 * @param Matcher $typeMatcher Matcher for the channel's type (e.g., $nPNone, $hueNone)
+	 * @param string $calcType Type for calc() function, defaults to 'number'
+	 * @return Alternative
+	 */
+	protected function createRelativeColorChannel(
+		string $channel,
+		Matcher $typeMatcher,
+		string $calcType = 'number'
+	): Alternative {
+		return new Alternative( [
+			new KeywordMatcher( [ $channel ] ),
+			$typeMatcher,
+			$this->calc( new KeywordMatcher( [ $channel ] ), $calcType )
+		] );
+	}
+
+	/**
+	 * Helper to build the Juxtaposition for relative color syntax.
+	 * @param Matcher $originColor Matcher for the base color
+	 * @param Matcher[] $componentMatchers Array of matchers for the color components
+	 * @param Matcher $optionalAlphaCalc Matcher for the optional alpha calculation
+	 * @return Juxtaposition
+	 */
+	protected function buildRelativeColorSyntax(
+		Matcher $originColor,
+		array $componentMatchers,
+		Matcher $optionalAlphaCalc
+	): Juxtaposition {
+		return new Juxtaposition( [
+			new KeywordMatcher( [ 'from' ] ),
+			$originColor,
+			...$componentMatchers,
+			$optionalAlphaCalc
+		] );
 	}
 
 	/**
